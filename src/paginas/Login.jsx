de6600008520864f30d67190400f8d0868e46652
Matias98cs <matias98cs@gmail.com>
2022-11-10 19:18:12 -0300

    cambios

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
   return (
     <>
         <div>
-            <h1 className='text-indigo-600 font-black text-6xl'>Inicia Sescion y Administra tus <span className='text-black'>Pacientes</span>
+            <h1 className='text-indigo-600 font-black text-6xl'>Inicia Sesión y Administra tus <span className='text-black'>Pacientes</span>
             </h1>
         </div>
         <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
